451ca87f8618f055958e86b1a687b6f1296b2d4c
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-01-31 11:17:45 +0100

    Planned on paper and begun section "input letter/word"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
 
 
+String guess= JOptionPane.showInputDialog("Guess your letter/ word (all lowercase, in english)");
 
+        if guess{}
 
         while (guessing==1){
 
