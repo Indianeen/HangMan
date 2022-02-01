a2da4140176dd31b4afc51c5cd6fde555d78fbf0
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-02-01 14:57:28 +0100

    Tried to structure up and proceed in the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,25 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+//Random word
+
+        //invisible
+
+        //Input
+        String guess= JOptionPane.showInputDialog("Guess your letter/ word (all lowercase, in english)");
+        //True/false
+
+
+        while (guessing==1) {
+            if (guess) {
+            }
+        }
+        //consequence
 
 
 
-String guess= JOptionPane.showInputDialog("Guess your letter/ word (all lowercase, in english)");
 
-        if guess{}
 
-        while (guessing==1){
 
         }
 
