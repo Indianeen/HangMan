46e48fc5de746bbaf045875d52b5a9bc16df65bf
Indianeen <axel.lundberg@elev.ga.ntig.se>
2022-02-08 15:13:34 +0100

    Tried to structure up and proceed in the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,22 +1,24 @@
 package com.company;
 
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 //Random word
-String selectedword =
+String[] selectedword;
+        selectedword = Random.;
         //invisible
-        for(int i=0; i<selectedword.length; i++){
-            selectedword[i] = '_';}
+        Arrays.fill(selectedword, '_');
             //Input
         String playerinput= JOptionPane.showInputDialog("Guess your letter/ word (all lowercase, in english)");
         //True/false
 
 
-        while (guessing==1) {
-            if (guess) {
+        while (int i = 0; guessing==1) {
+            if (playerinput) {
             }
         }
         //consequence
